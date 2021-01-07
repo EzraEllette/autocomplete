52396c36c887f8f0cfd1f1157febc414f54bcbce
Ezra Ellette <ezrasellette@gmail.com>
2021-01-07 12:27:14 -0600

    added Procfile

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
+app.set('port', (process.env.port || 3000));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
